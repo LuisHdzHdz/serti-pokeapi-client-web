93ed21168a56633cce39a9dfec012e0a010a2da7
Luis Alberto Hernandez Hernandez <luis.hernandez@r3d.com>
2023-12-26 17:37:50 -0600

    Cambios para mostrar resultados de la busqueda de cadena de evolucion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   title = 'pokeapi-angular-client'; 
   pokemons : any;
   results : any;
-  columnas: string[] = ['name'];
+  columnas: string[] = [];
   loadData = false;
   idEvChain: any;
   evolutionChain: any;
@@ -21,12 +21,14 @@ export class AppComponent {
   evolDetails : any[] = [];
   numberOfEvolutions : any;
   loadChain = false;
+  pokemonSprite: any;
 
   constructor(private httpService: ClientHttpService) {}
   
   @ViewChild(MatPaginator)  paginator! : MatPaginator;
 
   getAll() {
+    this.columnas =["name"];
     this.httpService.getAll().subscribe(
     (response) => { 
       this.pokemons = response;
@@ -37,8 +39,9 @@ export class AppComponent {
   }
 
   getEvolutionChain() {
-    this.httpService.getEvolutionChain(this.idEvChain).subscribe(
-    
+    this.columnas =["species_name"];
+    this.loadChain = false;
+    this.httpService.getEvolutionChain(this.idEvChain).subscribe(    
     (response) => { 
       this.evolutionChain = response;
       this.chain = this.evolutionChain.chain;
@@ -50,7 +53,6 @@ export class AppComponent {
       this.evolDetails.push({
         "species_name": this.chain.species.name,
         "min_level": !this.chain ? 1 : this.chain.min_level,
-        //"trigger_name": !this.chain ? null : this.chain.trigger.name,
         "item": !this.chain ? null : this.chain.item
       });
 
@@ -68,11 +70,23 @@ export class AppComponent {
       this.chain = this.chain['evolves_to'][0];
     }while(!!this.chain && this.chain.hasOwnProperty('evolves_to')){
       this.loadChain = true;
+      //console.log("URL sprire: "+this.getEvolutionChain());
       console.log("chain:"+JSON.stringify(this.evolDetails));
     }
 
     },
     (error) => { console.log(error); });
+  }
+
+  getSprite(idSprite: any){
+    let image;
+    this.httpService.getSprite(idSprite).subscribe(
+      (response) => { 
+        this.pokemonSprite = response;
+        image = this.pokemonSprite.sprites.front_default;
+        return image;
+      },
+      (error) => { console.log(error); });
   }
 
 }
--- src/app/client-http.service.ts
+++ src/app/client-http.service.ts
@@ -7,6 +7,7 @@ import { HttpClient } from  '@angular/common/http';
 export class ClientHttpService {
   private allUrl = 'http://localhost:8080/pokemon';
   private volutionUrl = 'http://localhost:8080/pokemon/evolution-chain?id=';
+  private spriteURl = "https://pokeapi.co/api/v2/pokemon/"
   constructor(private http: HttpClient) { }
   getAll() {
     return this.http.get(this.allUrl);
@@ -14,5 +15,9 @@ export class ClientHttpService {
 
   getEvolutionChain(id : any) {
     return this.http.get(this.volutionUrl+id);
+  }
+
+  getSprite(id : any){
+    return this.http.get(this.spriteURl+id);
   }
 }
